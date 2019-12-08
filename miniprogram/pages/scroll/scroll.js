24a068c2ed3d243698285ce13bbd1353c204d1e6
cc <c@freelancecn.com>
2019-12-08 11:21:33 +0800

    scrollmsg

---
+++ miniprogram/pages/scroll/scroll.js
@@ -0,0 +1,86 @@
+// pages/scroll/scroll.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    scrollMsg: [{
+      "avatar": "/images/user-unlogin.png",
+      "nickName": "爱国",
+      "name": "iPhone",
+      "createTime": "3分钟以前"
+    }, {
+      "avatar": "/images/user-unlogin.png",
+      "nickName": "建军",
+      "name": "iMac",
+      "createTime": "5分钟以前"
+    }, {
+      "avatar": "/images/user-unlogin.png",
+      "nickName": "富强",
+      "name": "Macbook Air",
+      "createTime": "30分钟以前"
+    }, {
+      "avatar": "/images/user-unlogin.png",
+      "nickName": "爱军",
+      "name": "Macbook Pro",
+      "createTime": "1小时以前"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
